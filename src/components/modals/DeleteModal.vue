4aabb6956fcc801a7f3c07c08e0b1edf68867899
eol nuha <olinuha08@hotmail.com>
2022-09-22 17:39:54 +0200

    Switched places of buttons

--- src/components/modals/DeleteModal.vue
+++ src/components/modals/DeleteModal.vue
@@ -27,9 +27,17 @@
             Are you sure you want to delete this?
           </h3>
           <button
+            @click="hideModal"
+            :disabled="isLoading"
+            type="button"
+            class="text-gray-500 bg-white w-20 h-10 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 inline-flex items-center justify-center dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600 mr-2"
+          >
+            Cancel
+          </button>
+          <button
             @click="deleteFunc"
             type="button"
-            class="text-white bg-red-600 w-20 h-10 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 text-sm font-medium rounded-lg inline-flex items-center justify-center px-5 py-2.5 mr-2"
+            class="text-white bg-red-600 w-20 h-10 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 text-sm font-medium rounded-lg inline-flex items-center justify-center px-5 py-2.5"
           >
             <IconC
               v-if="isLoading"
@@ -37,14 +45,6 @@
               iconClass="w-4 h-4 text-gray-200 animate-spin fill-gray-700"
             />
             <span v-else>Delete</span>
-          </button>
-          <button
-            @click="hideModal"
-            :disabled="isLoading"
-            type="button"
-            class="text-gray-500 bg-white w-20 h-10 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 inline-flex items-center justify-center dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-          >
-            Cancel
           </button>
         </div>
       </div>

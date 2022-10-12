be36054eca1edfc33d31a8fe0c73704c7b082739
eol nuha <olinuha08@hotmail.com>
2022-10-12 10:58:25 +0200

    Added users to dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,66 @@
 <template>
   <div class="flex-col flex bg-gray-200 dark:bg-gray-800 min-h-screen p-4">
-    <div class="bg-white dark:bg-gray-900 rounded-3xl my-5 py-8 relative px-5">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <div class="bg-white dark:bg-gray-900 p-5 rounded-xl relative">
+        <h3 class="text-gray-900 dark:text-white text-2xl mb-2">Products</h3>
+        <router-link class="text-blue-500 block mb-2" :to="{ name: 'products' }"
+          >Manage Products</router-link
+        >
+        <router-link
+          class="text-blue-500 block mb-2"
+          :to="{ name: 'new-product' }"
+          >Create Product</router-link
+        >
+        <IconC
+          iconName="TagIcon"
+          iconClass="w-12 h-12 absolute right-[5px] bottom-[5px]  text-blue-500/50"
+        />
+      </div>
+      <div class="bg-white dark:bg-gray-900 p-5 rounded-xl relative">
+        <h3 class="text-gray-900 dark:text-white text-2xl mb-2">Sales</h3>
+        <router-link class="text-blue-500 block mb-2" :to="{ name: 'sales' }"
+          >Manage Sales</router-link
+        >
+        <router-link class="text-blue-500 block mb-2" :to="{ name: 'new-sale' }"
+          >Create Sale</router-link
+        >
+        <IconC
+          iconName="ShoppingCartIcon"
+          iconClass="w-12 h-12 absolute right-[5px] bottom-[5px]  text-blue-500/50"
+        />
+      </div>
+      <div class="bg-white dark:bg-gray-900 p-5 rounded-xl relative">
+        <h3 class="text-gray-900 dark:text-white text-2xl mb-2">Purchases</h3>
+        <router-link
+          class="text-blue-500 block mb-2"
+          :to="{ name: 'purchases' }"
+          >Manage Purchases</router-link
+        >
+        <router-link
+          class="text-blue-500 block mb-2"
+          :to="{ name: 'new-purchase' }"
+          >Create Purchase</router-link
+        >
+        <IconC
+          iconName="BuildingStorefrontIcon"
+          iconClass="w-12 h-12 absolute right-[5px] bottom-[5px]  text-blue-500/50"
+        />
+      </div>
+      <div class="bg-white dark:bg-gray-900 p-5 rounded-xl relative">
+        <h3 class="text-gray-900 dark:text-white text-2xl mb-2">Users</h3>
+        <router-link class="text-blue-500 block mb-2" :to="{ name: 'users' }"
+          >Manage Users</router-link
+        >
+        <router-link class="text-blue-500 block mb-2" :to="{ name: 'new-user' }"
+          >Create User</router-link
+        >
+        <IconC
+          iconName="UserGroupIcon"
+          iconClass="w-12 h-12 absolute right-[5px] bottom-[5px]  text-blue-500/50"
+        />
+      </div>
+    </div>
+    <div class="bg-white dark:bg-gray-900 rounded-xl my-5 py-8 relative px-5">
       <h3 class="text-gray-900 dark:text-white text-3xl">Sales</h3>
       <p class="text-gray-500 dark:text-gray-400">
         Total day sales over the last seven days
@@ -13,7 +73,7 @@
         />
       </div>
     </div>
-    <div class="bg-white dark:bg-gray-900 rounded-3xl my-5 py-8 relative px-5">
+    <div class="bg-white dark:bg-gray-900 rounded-xl py-8 relative px-5">
       <h3 class="text-gray-900 dark:text-white text-3xl">Purchases</h3>
       <p class="text-gray-500 dark:text-gray-400">
         Total day purchases over the last seven days

6a951eca521fd8231a60fd9fee87df845a092ba9
eol nuha <olinuha08@hotmail.com>
2022-11-16 17:17:11 +0100

    Added pdf generator to sale and purchase details

--- src/views/purchases/PurchaseDetailsView.vue
+++ src/views/purchases/PurchaseDetailsView.vue
@@ -3,7 +3,10 @@
     <div class="flex items-center justify-between flex-wrap gap-2">
       <div class="flex items-center gap-2"></div>
       <div class="flex flex-row items-center gap-2">
-        <button class="red-gradient-btn inline-flex items-center text-center">
+        <button
+          class="red-gradient-btn inline-flex items-center text-center"
+          @click="download"
+        >
           <div class="inline-flex flex-row" role="status" v-if="isPdfLoading">
             <IconC
               iconType="custom"
@@ -28,119 +31,115 @@
       class="bg-white dark:bg-neutral-900 rounded-3xl my-5 py-8 relative px-10"
     >
       <OverlayC v-if="isLoading" />
-      <div class="flex items-center flex-row justify-between">
-        <h2 class="text-gray-700 dark:text-gray-300 text-3xl font-extrabold">
-          {{ $t("purchase") }} #{{ $route.params.purchaseId }}
-        </h2>
-        <p class="text-gray-700 dark:text-gray-300">
-          {{ $t("date") }}: {{ purchase.dateCreated.substring(0, 10) }}
-        </p>
-      </div>
-      <div class="flex flex-row pt-5">
-        <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
-          <table class="text-gray-700 dark:text-gray-400 text-sm w-full">
+      <div id="content">
+        <div class="flex items-center flex-row justify-between">
+          <h2 class="text-gray-700 dark:text-gray-300 text-3xl font-extrabold">
+            {{ $t("purchase") }} #{{ $route.params.purchaseId }}
+          </h2>
+          <p class="text-gray-700 dark:text-gray-300 mr-2">
+            {{ $t("date") }}: {{ purchase.dateCreated.substring(0, 10) }}
+          </p>
+        </div>
+        <div class="flex flex-row pt-5">
+          <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
+            <table class="text-gray-700 dark:text-gray-400 text-sm w-full">
+              <tbody>
+                <tr>
+                  <td class="py-2">{{ $t("sellerName") }}</td>
+                  <td class="text-right py-2">{{ purchase.sellerName }}</td>
+                </tr>
+                <tr>
+                  <td class="py-2">{{ $t("invoiceNumber") }}</td>
+                  <td class="text-right py-2">
+                    {{ purchase.sellerInvoiceNumber }}
+                  </td>
+                </tr>
+                <tr>
+                  <td class="py-2">{{ $t("fiscalNumber") }}</td>
+                  <td class="text-right py-2">
+                    {{ purchase.sellerFiscalNumber }}
+                  </td>
+                </tr>
+                <tr>
+                  <td class="py-2">{{ $t("taxNumber") }}</td>
+                  <td class="text-right py-2">
+                    {{ purchase.sellerTaxNumber }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div
+          class="overflow-x-auto relative sm:rounded-lg my-5 scrollbar-style"
+        >
+          <table
+            class="w-full text-sm text-left text-gray-700 dark:text-gray-400 relative my-5"
+          >
+            <thead
+              class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-neutral-700 dark:text-gray-400"
+            >
+              <tr>
+                <th scope="col" class="py-3 px-3">{{ $t("productName") }}</th>
+                <th scope="col" class="py-3 px-3">{{ $t("barcode") }}</th>
+                <th scope="col" class="py-3 px-3">{{ $t("stock") }}</th>
+                <th scope="col" class="py-3 px-3">
+                  {{ $t("purchasedPrice") }}
+                </th>
+                <!-- <th scope="col" class="py-3 px-3">{{ $t("priceWithoutTax") }}</th> -->
+                <th scope="col" class="py-3 px-3">{{ $t("tax") }}</th>
+                <th scope="col" class="py-3 px-3">{{ $t("sellingPrice") }}</th>
+                <th scope="col" class="py-3 px-3 text-right">
+                  {{ $t("total") }}
+                </th>
+              </tr>
+            </thead>
             <tbody>
-              <tr>
-                <td class="py-2">{{ $t("sellerName") }}</td>
-                <td class="text-right py-2">{{ purchase.sellerName }}</td>
-              </tr>
-              <tr>
-                <td class="py-2">{{ $t("invoiceNumber") }}</td>
-                <td class="text-right py-2">
-                  {{ purchase.sellerInvoiceNumber }}
-                </td>
-              </tr>
-              <tr>
-                <td class="py-2">{{ $t("fiscalNumber") }}</td>
-                <td class="text-right py-2">
-                  {{ purchase.sellerFiscalNumber }}
-                </td>
-              </tr>
-              <tr>
-                <td class="py-2">{{ $t("taxNumber") }}</td>
-                <td class="text-right py-2">
-                  {{ purchase.sellerTaxNumber }}
-                </td>
-              </tr>
+              <template v-for="item in purchase.purchaseItems" :key="item.id">
+                <tr class="bg-white dark:bg-neutral-900">
+                  <td class="py-3 px-3">
+                    {{ item.product.name }}
+                  </td>
+                  <td class="py-3 px-3">{{ item.product.barcode }}</td>
+                  <td class="py-3 px-3">x {{ item.product.stock }}</td>
+                  <td class="py-3 px-3">{{ item.product.purchasedPrice }} €</td>
+                  <!-- <td class="py-3 px-3">{{ item.priceWithoutTax }} €</td> -->
+                  <td class="py-3 px-3">
+                    {{ item.taxAmount }} € ({{ item.product.tax }}%)
+                  </td>
+                  <td class="py-3 px-3">{{ item.product.sellingPrice }} €</td>
+                  <td class="py-3 px-3 text-right">{{ item.totalAmount }} €</td>
+                </tr>
+              </template>
             </tbody>
           </table>
         </div>
-      </div>
-      <div class="overflow-x-auto relative sm:rounded-lg my-5 scrollbar-style">
-        <table
-          class="w-full text-sm text-left text-gray-700 dark:text-gray-400 relative my-5"
-        >
-          <thead
-            class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-neutral-700 dark:text-gray-400"
-          >
-            <tr>
-              <th scope="col" class="py-3 px-6">ID</th>
-              <th scope="col" class="py-3 px-6">{{ $t("productName") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("barcode") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("stock") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("purchasedPrice") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("priceWithoutTax") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("tax") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("sellingPrice") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("total") }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-for="item in purchase.purchaseItems" :key="item.id">
-              <tr class="bg-white dark:bg-neutral-900">
-                <td class="py-3 px-6">{{ item.product.id }}</td>
-                <td
-                  class="py-3 px-6 text-black dark:text-white cursor-pointer"
-                  @click="
-                    $router.push({
-                      name: 'product-view',
-                      params: { productId: item.product.id },
-                    })
-                  "
-                >
-                  {{ item.product.name }}
-                </td>
-                <td class="py-3 px-6">{{ item.product.barcode }}</td>
-                <td class="py-3 px-6">x {{ item.product.stock }}</td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ item.product.purchasedPrice }} €
-                </td>
-                <td class="py-3 px-6 max-w-xs">{{ item.priceWithoutTax }} €</td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ item.taxAmount }} € ({{ item.product.tax }}%)
-                </td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ item.product.sellingPrice }} €
-                </td>
-                <td class="py-3 px-6 max-w-xs">{{ item.totalAmount }} €</td>
-              </tr>
-            </template>
-          </tbody>
-        </table>
-      </div>
-      <hr class="border-gray-500" />
-      <div class="flex flex-row justify-end p-5">
-        <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
-          <table class="text-gray-700 dark:text-gray-300 w-full">
-            <tbody>
-              <tr>
-                <td class="py-2">{{ $t("subTotal") }}</td>
-                <td class="text-right py-2">{{ purchase.subTotalAmount }} €</td>
-              </tr>
-              <tr v-for="item in taxes" :key="item.settingsValue">
-                <td class="py-2 uppercase">
-                  {{ $t("tax") }} ({{ item.settingsValue }}%)
-                </td>
-                <td class="text-right py-2">
-                  {{ getTaxValue(purchase.taxes, item.settingsAlias) }} €
-                </td>
-              </tr>
-              <tr class="font-bold text-xl">
-                <td class="py-2">{{ $t("total") }}</td>
-                <td class="text-right py-2">{{ purchase.totalAmount }} €</td>
-              </tr>
-            </tbody>
-          </table>
+        <hr class="border-gray-300 dark:border-gray-600" />
+        <div class="flex flex-row justify-end p-5">
+          <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
+            <table class="text-gray-700 dark:text-gray-300 w-full">
+              <tbody>
+                <tr>
+                  <td class="py-2">{{ $t("subTotal") }}</td>
+                  <td class="text-right py-2">
+                    {{ purchase.subTotalAmount }} €
+                  </td>
+                </tr>
+                <tr v-for="item in taxes" :key="item.settingsValue">
+                  <td class="py-2 uppercase">
+                    {{ $t("tax") }} ({{ item.settingsValue }}%)
+                  </td>
+                  <td class="text-right py-2">
+                    {{ getTaxValue(purchase.taxes, item.settingsAlias) }} €
+                  </td>
+                </tr>
+                <tr class="font-bold text-xl">
+                  <td class="py-2">{{ $t("total") }}</td>
+                  <td class="text-right py-2">{{ purchase.totalAmount }} €</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -148,10 +147,12 @@
 </template>
 
 <script>
+import { jsPDF } from "jspdf";
 export default {
   data() {
     return {
       isLoading: true,
+      isPdfLoading: false,
     };
   },
   computed: {
@@ -178,6 +179,28 @@ export default {
   methods: {
     getTaxValue(arr, alias) {
       return arr.find((x) => x.taxAlias === alias)?.taxValue || 0;
+    },
+    async download() {
+      this.isPdfLoading = true;
+      const purchaseTxt = this.$t("purchase").toLowerCase();
+      const purchaseId = this.$route.params.purchaseId;
+      let doc = new jsPDF();
+
+      let elementHTML = document.querySelector("#content");
+
+      await doc.html(elementHTML, {
+        callback: function (doc) {
+          // Save the PDF
+          doc.save(`${purchaseTxt}-${purchaseId}.pdf`);
+        },
+        margin: [10, 10, 10, 10],
+        autoPaging: "text",
+        x: 0,
+        y: 0,
+        width: 190,
+        windowWidth: 800,
+      });
+      this.isPdfLoading = false;
     },
   },
 };
--- src/views/sales/SaleDetailsView.vue
+++ src/views/sales/SaleDetailsView.vue
@@ -3,7 +3,10 @@
     <div class="flex items-center justify-between flex-wrap gap-2">
       <div class="flex items-center gap-2"></div>
       <div class="flex flex-row items-center gap-2">
-        <button class="red-gradient-btn inline-flex items-center text-center">
+        <button
+          class="red-gradient-btn inline-flex items-center text-center"
+          @click="download"
+        >
           <div class="inline-flex flex-row" role="status" v-if="isPdfLoading">
             <IconC
               iconType="custom"
@@ -25,94 +28,99 @@
       </div>
     </div>
     <div
-      class="bg-white dark:bg-neutral-900 rounded-3xl my-5 py-8 relative px-10"
+      class="bg-white dark:bg-neutral-900 rounded-3xl relative my-5 px-10 py-8"
+      v-show="!isPrint"
     >
-      <OverlayC v-if="isLoading" />
-      <div class="flex items-center flex-row justify-between">
-        <h2 class="text-gray-700 dark:text-gray-300 text-3xl font-extrabold">
-          {{ $t("sale") }} #{{ $route.params.saleId }}
-        </h2>
-        <p class="text-gray-700 dark:text-gray-300">
-          {{ $t("date") }}: {{ sale.dateCreated.substring(0, 10) }}
-        </p>
-      </div>
-      <div class="overflow-x-auto relative sm:rounded-lg my-5 scrollbar-style">
-        <table
-          class="w-full text-sm text-left text-gray-700 dark:text-gray-400 relative my-5"
+      <div id="content">
+        <OverlayC v-if="isLoading" />
+        <div class="flex items-center flex-row justify-between">
+          <h2 class="text-gray-700 dark:text-gray-300 text-3xl font-extrabold">
+            {{ $t("sale") }} #{{ $route.params.saleId }}
+          </h2>
+          <p class="text-gray-700 dark:text-gray-300 mr-3">
+            {{ $t("date") }}: {{ sale.dateCreated.substring(0, 10) }}
+          </p>
+        </div>
+        <div
+          class="overflow-x-auto relative sm:rounded-lg my-5 scrollbar-style"
         >
-          <thead
-            class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-neutral-700 dark:text-gray-400"
+          <table
+            class="w-full text-sm text-left text-gray-700 dark:text-gray-400 relative my-5"
           >
-            <tr>
-              <th scope="col" class="py-3 px-6">ID</th>
-              <th scope="col" class="py-3 px-6">{{ $t("productName") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("barcode") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("quantity") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("price") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("priceWithoutTax") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("tax") }}</th>
-              <th scope="col" class="py-3 px-6">{{ $t("total") }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-for="item in sale.saleItems" :key="item.id">
-              <tr class="bg-white dark:bg-neutral-900">
-                <td class="py-3 px-6">{{ item.product.id }}</td>
-                <td class="py-3 px-6">
-                  {{ item.product.name }}
-                </td>
-                <td class="py-3 px-6">{{ item.product.barcode }}</td>
-                <td class="py-3 px-6">x {{ item.quantity }}</td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ item.product.sellingPrice }} €
-                </td>
-                <td class="py-3 px-6 max-w-xs">{{ item.priceWithoutTax }} €</td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ item.taxAmount }} € ({{ item.product.tax }}%)
-                </td>
-                <td class="py-3 px-6 max-w-xs">
-                  {{ (item.product.sellingPrice * item.quantity).toFixed(2) }} €
-                </td>
-              </tr>
-            </template>
-          </tbody>
-        </table>
-        <hr class="border-gray-500" />
-      </div>
-      <div class="flex flex-row justify-end p-5">
-        <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
-          <table class="text-gray-700 dark:text-gray-300 w-full">
-            <tbody>
+            <thead
+              class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-neutral-700 dark:text-gray-400"
+            >
               <tr>
-                <td class="py-2 uppercase">{{ $t("subTotal") }}</td>
-                <td class="text-right py-2">{{ sale.subTotalAmount }} €</td>
+                <th scope="col" class="py-3 px-6">{{ $t("productName") }}</th>
+                <th scope="col" class="py-3 px-6">{{ $t("barcode") }}</th>
+                <th scope="col" class="py-3 px-6">{{ $t("quantity") }}</th>
+                <th scope="col" class="py-3 px-6">{{ $t("price") }}</th>
+                <th scope="col" class="py-3 px-6">{{ $t("tax") }}</th>
+                <th scope="col" class="py-3 px-6 text-right">
+                  {{ $t("total") }}
+                </th>
               </tr>
-              <tr v-for="item in taxes" :key="item.settingsValue">
-                <td class="py-2 uppercase">
-                  {{ $t("tax") }} ({{ item.settingsValue }}%)
-                </td>
-                <td class="text-right py-2">
-                  {{ getTaxValue(sale.taxes, item.settingsAlias) }} €
-                </td>
-              </tr>
-              <tr class="font-bold text-xl">
-                <td class="py-2 uppercase">{{ $t("total") }}</td>
-                <td class="text-right py-2">{{ sale.totalAmount }} €</td>
-              </tr>
+            </thead>
+            <tbody>
+              <template v-for="item in sale.saleItems" :key="item.id">
+                <tr class="bg-white dark:bg-neutral-900">
+                  <td class="py-3 px-6">
+                    {{ item.product.name }}
+                  </td>
+                  <td class="py-3 px-6">{{ item.product.barcode }}</td>
+                  <td class="py-3 px-6">x {{ item.quantity }}</td>
+                  <td class="py-3 px-6">{{ item.product.sellingPrice }} €</td>
+                  <td class="py-3 px-6">
+                    {{ item.taxAmount }} € ({{ item.product.tax }}%)
+                  </td>
+                  <td class="py-3 px-6 text-right">
+                    {{ (item.product.sellingPrice * item.quantity).toFixed(2) }}
+                    €
+                  </td>
+                </tr>
+              </template>
             </tbody>
           </table>
+          <hr class="border-gray-300 dark:border-gray-600" />
+        </div>
+        <div class="flex flex-row justify-end p-5">
+          <div class="basis-3/4 md:basis-1/2 lg:basis-1/4">
+            <table class="text-gray-700 dark:text-gray-300 w-full">
+              <tbody>
+                <tr>
+                  <td class="py-2 uppercase">{{ $t("subTotal") }}</td>
+                  <td class="text-right py-2">{{ sale.subTotalAmount }} €</td>
+                </tr>
+                <tr v-for="item in taxes" :key="item.settingsValue">
+                  <td class="py-2 uppercase">
+                    {{ $t("tax") }} ({{ item.settingsValue }}%)
+                  </td>
+                  <td class="text-right py-2">
+                    {{ getTaxValue(sale.taxes, item.settingsAlias) }} €
+                  </td>
+                </tr>
+                <tr class="font-bold text-xl">
+                  <td class="py-2 uppercase">{{ $t("total") }}</td>
+                  <td class="text-right py-2">{{ sale.totalAmount }} €</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
+    <iframe v-if="isPrint" id="iframe" width="100%" height="1000px"></iframe>
   </div>
 </template>
 
 <script>
+import { jsPDF } from "jspdf";
 export default {
   data() {
     return {
       isLoading: true,
       isPdfLoading: false,
+      isPrint: false,
     };
   },
   computed: {
@@ -136,6 +144,34 @@ export default {
   methods: {
     getTaxValue(arr, alias) {
       return arr.find((x) => x.taxAlias === alias)?.taxValue || 0;
+    },
+    async download() {
+      this.isPdfLoading = true;
+      const saleTxt = this.$t("sale").toLowerCase();
+      const saleId = this.$route.params.saleId;
+      let doc = new jsPDF();
+
+      let elementHTML = document.querySelector("#content");
+
+      await doc.html(elementHTML, {
+        callback: function (doc) {
+          // Save the PDF
+          doc.save(`${saleTxt}-${saleId}.pdf`);
+        },
+        margin: [10, 10, 10, 10],
+        autoPaging: "text",
+        x: 0,
+        y: 0,
+        width: 190,
+        windowWidth: 800,
+      });
+      this.isPdfLoading = false;
+      // this.isPrint = true;
+      //TODO: Create later
+      // let iframe = document.getElementById("iframe");
+      // document.body.appendChild(iframe);
+
+      // iframe.src = doc.output("datauristring");
     },
   },
 };

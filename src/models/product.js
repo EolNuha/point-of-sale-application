1754e22cf461c783cba57ba955e326f560536214
eol nuha <olinuha08@hotmail.com>
2023-11-12 12:09:40 +0100

    [UPDATE] fixed issue

--- src/models/product.js
+++ src/models/product.js
@@ -23,7 +23,7 @@ class Product {
     this.purchased_price_wo_tax = data.purchased_price_wo_tax || null;
     this.purchased_price = data.purchased_price || null;
     this.selling_price = data.selling_price || null;
-    this.expiration_date = data.expiration_date.substring(0, 10) || null;
+    this.expiration_date = data.expiration_date?.substring(0, 10) || null;
     this.date_created = data.date_created || null;
     this.date_modified = data.date_modified || null;
   }

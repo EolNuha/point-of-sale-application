0ea08cf612f17a5d87a3377c969bfb92a037a904
eol nuha <olinuha08@hotmail.com>
2023-11-16 23:30:20 +0100

    [UPDATE] user.py camelCase -> snake_case

--- resources/website/jsonify/user.py
+++ resources/website/jsonify/user.py
@@ -1,15 +1,16 @@
 def getUserDict(item):
     return {
         "id": item.id,
-        "firstName": item.first_name,
-        "lastName": item.last_name,
+        "first_name": item.first_name,
+        "last_name": item.last_name,
         "username": item.username,
-        "userRole": item.user_role,
+        "user_role": item.user_role,
         "email": item.email,
         "active": item.active,
-        "dateCreated": item.date_created.strftime('%d.%m.%Y, %H:%M:%S'),
-        "dateModified": item.date_modified.strftime('%d.%m.%Y, %H:%M:%S'),
+        "date_created": item.date_created.strftime("%d.%m.%Y, %H:%M:%S"),
+        "date_modified": item.date_modified.strftime("%d.%m.%Y, %H:%M:%S"),
     }
+
 
 def getUsersList(users):
     return [getUserDict(i) for i in users]

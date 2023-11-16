2e02343dc1a203106389bf7fec1c7fb7cdd751f0
eol nuha <olinuha08@hotmail.com>
2023-11-16 23:30:12 +0100

    [UPDATE] settings.py camelCase -> snake_case

--- resources/website/jsonify/settings.py
+++ resources/website/jsonify/settings.py
@@ -9,9 +9,9 @@ def getTaxDict(item):
     ctx.rounding = decimal.ROUND_HALF_UP
     return {
         "id": item.id,
-        "taxName": item.tax_name,
-        "taxAlias": item.tax_alias,
-        "taxValue": float(Decimal(item.tax_value).quantize(TWOPLACES)),
+        "tax_name": item.tax_name,
+        "tax_alias": item.tax_alias,
+        "tax_value": float(Decimal(item.tax_value).quantize(TWOPLACES)),
     }
 
 

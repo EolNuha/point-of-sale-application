dbe24e1520b999a1ba0354a77cf50cdcb2a906fa
eol nuha <olinuha08@hotmail.com>
2024-01-05 23:17:41 +0100

    settings jsonify update

--- resources/website/api_models/settings.py
+++ resources/website/api_models/settings.py
@@ -2,19 +2,25 @@ from flask_restx import fields
 
 from .. import swagger
 
-settings_model = swagger.model("Settings", {
-    "id": fields.Integer,
-    "settings_name": fields.String,
-    "settings_alias": fields.String,
-    "settings_type": fields.String,
-    "settings_value": fields.String,
-    "date_created": fields.String,
-    "date_modified": fields.String,
-})
+settings_model = swagger.model(
+    "Settings",
+    {
+        "id": fields.Integer,
+        "settings_name": fields.String,
+        "settings_alias": fields.String,
+        "settings_type": fields.String,
+        "settings_value": fields.String,
+        "date_created": fields.String,
+        "date_modified": fields.String,
+    },
+)
 
-settings_input_model = swagger.model("SettingsInput", {
-    "settings_name": fields.String,
-    "settings_alias": fields.String,
-    "settings_type": fields.String,
-    "settings_value": fields.String,
-})
+settings_input_model = swagger.model(
+    "SettingsInput",
+    {
+        "settings_name": fields.String,
+        "settings_alias": fields.String,
+        "settings_type": fields.String,
+        "settings_value": fields.String,
+    },
+)

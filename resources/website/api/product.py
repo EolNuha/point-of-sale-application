32160f484837ebd019dd0124bc6eedfaee217867
eol nuha <olinuha08@hotmail.com>
2024-03-12 21:29:56 +0100

    API Products Excel Upload

--- resources/website/api/product.py
+++ resources/website/api/product.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from website.models.product import Product
 from website.jsonify.product import getProductsList, getProductsExcelList
 from website.helpers import get_page_range
@@ -13,14 +14,29 @@ from website.api_models.product import (
 from website.api_models.pagination import pagination
 from flask_restx import Namespace, Resource, reqparse
 from werkzeug.exceptions import BadRequest
+from openpyxl import load_workbook
+from werkzeug.datastructures import FileStorage
+import decimal
 
 product_rest = Namespace("Product")
 parser = reqparse.RequestParser()
+file_parser = reqparse.RequestParser()
 parser.add_argument("page", type=int, default=1)
 parser.add_argument("per_page", type=int, default=20)
 parser.add_argument("search", type=str, default="")
 parser.add_argument("sort_column", type=str, default="id")
 parser.add_argument("sort_dir", type=str, default="desc")
+file_parser.add_argument(
+    "file",
+    type=FileStorage,
+    location="files",
+    required=False,
+    help="File upload",
+)
+
+Decimal = decimal.Decimal
+FOURPLACES = Decimal(10) ** -4
+TWOPLACES = Decimal(10) ** -2
 
 
 @product_rest.route("products")
@@ -176,6 +192,101 @@ class GetProductsExcel(Resource):
         )
 
         return getProductsExcelList(items), 200
+
+    @product_rest.expect(file_parser)
+    def put(self):
+        args = file_parser.parse_args()
+        file = args["file"]
+
+        wb = load_workbook(file)
+        ws = wb.active
+
+        arr = [
+            "Barkodi",
+            "Emri",
+            "Sasia",
+            "TVSH",
+            "Çmimi i blerjes",
+            "Çmimi i shitjes",
+            "Njesia matese",
+        ]
+
+        if ws.max_row > 501:
+            raise BadRequest("Numri maksimal i produkteve eshte 500!")
+
+        for index, row in enumerate(ws.iter_rows(values_only=True)):
+            if index > 0:
+                break
+            for idx, value in enumerate(row):
+                if value.lower() != arr[idx].lower():
+                    raise BadRequest(f'Kolona "{arr[idx]}" eshte gabim!')
+
+        if ws.max_row == 1:
+            raise BadRequest("Asnje produkt nuk eshte shtuar!")
+
+        for index, row in enumerate(ws.iter_rows(values_only=True)):
+            if index == 0:
+                continue
+            self.validate_inputs(row)
+            try:
+                purchased_price_wo_tax = Decimal(row[4])
+                tax_percentage = Decimal(row[3]) / 100
+                tax_amount = Decimal(purchased_price_wo_tax * tax_percentage).quantize(
+                    FOURPLACES
+                )
+                product_purchased_price = Decimal(
+                    purchased_price_wo_tax + tax_amount
+                ).quantize(FOURPLACES)
+
+                product = Product(
+                    barcode=row[0],
+                    name=row[1],
+                    stock=row[2],
+                    tax=float(row[3]),
+                    purchased_price_wo_tax=purchased_price_wo_tax,
+                    purchased_price=product_purchased_price,
+                    selling_price=Decimal(row[5]).quantize(FOURPLACES),
+                    measure=row[6],
+                    date_created=datetime.now(),
+                    date_modified=datetime.now(),
+                )
+
+                db.session.add(product)
+            except Exception as e:
+                print(e)
+                return "Something went wrong!", 500
+
+        db.session.commit()
+
+        return jsonify(arr)
+
+    def validate_inputs(self, row):
+        arr = [
+            "Barkodi",
+            "Emri",
+            "Sasia",
+            "TVSH",
+            "Çmimi i blerjes",
+            "Çmimi i shitjes",
+            "Njesia matese",
+        ]
+        for i in range(0, 7):
+            if not row[i] and row[i] != 0:
+                raise BadRequest(
+                    f"Fusha {arr[i]} {row[i]} {type(row[i])} nuk mund te jete e zbrazet!"
+                )
+
+            if i not in [1, 6]:
+                try:
+                    if i == 3 and float(row[i]) not in [0, 8, 18]:
+                        raise BadRequest("Jo Valid!")
+                    else:
+                        float(row[i])
+                except Exception as e:
+                    raise BadRequest(f"Vlera {row[i]} nuk eshte {arr[i]} valid!")
+
+            if i == 6 and row[i].lower() not in ["kg", "pcs", "liter"]:
+                raise BadRequest(f"Njesia matese duhet te jete pcs, kg, ose liter")
 
 
 @product_rest.route("products/<int:productId>")
--- resources/website/models/product.py
+++ resources/website/models/product.py
@@ -2,21 +2,29 @@ from .. import db
 from datetime import datetime
 from sqlalchemy import Index
 
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     barcode = db.Column(db.Integer, unique=True)
     name = db.Column(db.String(200), unique=True)
     stock = db.Column(db.Numeric(precision=10, scale=4), default=0)
-    tax=db.Column(db.Integer, default=0)
+    tax = db.Column(db.Integer, default=0)
     purchased_price_wo_tax = db.Column(db.Numeric(precision=10, scale=4), default=0)
     purchased_price = db.Column(db.Numeric(precision=10, scale=4), default=0)
     selling_price = db.Column(db.Numeric(precision=10, scale=4), default=0)
-    measure = db.Column(db.String(50))
+    measure = db.Column(db.Enum("pcs", "kg", "liter"), default="pcs")
     expiration_date = db.Column(db.DateTime, default=None)
     date_created = db.Column(db.DateTime, default=datetime.now())
     date_modified = db.Column(db.DateTime, default=datetime.now())
 
-Index('ix_products', Product.name.asc(), Product.barcode.asc(), Product.id.asc(), unique=True)
-Index('ix_product_name', Product.name.asc(), unique=True)
-Index('ix_product_id', Product.id.asc(), unique=True)
-Index('ix_product_barcode', Product.barcode.asc(), unique=True)
+
+Index(
+    "ix_products",
+    Product.name.asc(),
+    Product.barcode.asc(),
+    Product.id.asc(),
+    unique=True,
+)
+Index("ix_product_name", Product.name.asc(), unique=True)
+Index("ix_product_id", Product.id.asc(), unique=True)
+Index("ix_product_barcode", Product.barcode.asc(), unique=True)
